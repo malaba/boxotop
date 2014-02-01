4e520bba019654417afe009508d0cba8889d43b7
Pascal Drouilly <github.com@drouilly.net>
2014-02-02 00:02:35 +0100

    - uncrustify all source - removed landscape, added UpsideDown - added .xccheckout file

--- boxotop/DetailViewController.m
+++ boxotop/DetailViewController.m
@@ -16,18 +16,16 @@
 
 #pragma mark - Managing the detail item
 
-- (void)setDetailItem:(id)newDetailItem
-{
+- (void)setDetailItem:(id)newDetailItem {
     if (_detailItem != newDetailItem) {
         _detailItem = newDetailItem;
-        
+
         // Update the view.
         [self configureView];
     }
 }
 
-- (void)configureView
-{
+- (void)configureView {
     // Update the user interface for the detail item.
 
     if (self.detailItem) {
@@ -35,15 +33,13 @@
     }
 }
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
     [self configureView];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
