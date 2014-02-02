c58b63fb4e6e21e1afa8e730b44b4c9dd12be51f
Pascal Drouilly <github.com@drouilly.net>
2014-02-02 16:24:12 +0100

    added TableViewCell class "Movie_Cell"

---
+++ boxotop/Cell/Movie_Cell.h
@@ -0,0 +1,13 @@
+//
+//  Movie_Cell.h
+//  boxotop
+//
+//  Created by Pascal Drouilly on 02/02/14.
+//  Copyright (c) 2014 drouilly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Movie_Cell : UITableViewCell
+
+@end
---
+++ boxotop/Cell/Movie_Cell.m
@@ -0,0 +1,27 @@
+//
+//  Movie_Cell.m
+//  boxotop
+//
+//  Created by Pascal Drouilly on 02/02/14.
+//  Copyright (c) 2014 drouilly. All rights reserved.
+//
+
+#import "Movie_Cell.h"
+
+@implementation Movie_Cell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
