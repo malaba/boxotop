bbaf12cf92b29c212e9cf80d88cee22fb3c27f9d
Pascal Drouilly <github.com@drouilly.net>
2014-02-02 15:23:33 +0100

    forgot to checkin the +Private files

---
+++ boxotop/DetailViewController+Private.h
@@ -0,0 +1,15 @@
+//
+//  DetailViewController+Private.h
+//  boxotop
+//
+//  Created by Pascal Drouilly on 02/02/14.
+//  Copyright (c) 2014 drouilly. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+- (void)configureView;
+
+@end
---
+++ boxotop/MasterViewController+Private.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController+Private.h
+//  boxotop
+//
+//  Created by Pascal Drouilly on 02/02/14.
+//  Copyright (c) 2014 drouilly. All rights reserved.
+//
+
+#import "MasterViewController.h"
+
+@interface MasterViewController ()
+
+@property (strong, nonatomic) NSMutableArray *model;
+
+@end
