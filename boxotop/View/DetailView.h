e9a6792afc6468e9c664f2cc5ed85421c375e1ae
Pascal Drouilly <github.com@drouilly.net>
2014-02-03 18:11:42 +0100

    added DetailView class and xib

---
+++ boxotop/View/DetailView.h
@@ -0,0 +1,13 @@
+//
+//  DetailView.h
+//  boxotop
+//
+//  Created by Pascal Drouilly on 03/02/14.
+//  Copyright (c) 2014 drouilly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailView : UIView
+
+@end
---
+++ boxotop/View/DetailView.m
@@ -0,0 +1,31 @@
+//
+//  DetailView.m
+//  boxotop
+//
+//  Created by Pascal Drouilly on 03/02/14.
+//  Copyright (c) 2014 drouilly. All rights reserved.
+//
+
+#import "DetailView.h"
+
+@implementation DetailView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
